e80494962f4a911ba3c3ec74e75f0a6c8aeac36b
Svitlana Moiseyenko <alexandrovna.sveta@gmail.com>
2016-11-08 01:27:19 +0200

    Move to Swift3.

---
+++ Example/build/Pods.build/Debug-iphonesimulator/ASAutoResizingTextView.build/Objects-normal/x86_64/ASAutoResizingTextView-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/svitlana/CocoaPods/ASAutoResizingTextView/Example/Pods/Target\ Support\ Files/ASAutoResizingTextView/ASAutoResizingTextView-dummy.m \
+  /Users/svitlana/CocoaPods/ASAutoResizingTextView/Example/Pods/Target\ Support\ Files/ASAutoResizingTextView/ASAutoResizingTextView-prefix.pch
---
+++ Example/build/Pods.build/Debug-iphonesimulator/ASAutoResizingTextView.build/Objects-normal/x86_64/ASAutoResizingTextView_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/svitlana/CocoaPods/ASAutoResizingTextView/Example/build/Pods.build/Debug-iphonesimulator/ASAutoResizingTextView.build/DerivedSources/ASAutoResizingTextView_vers.c
